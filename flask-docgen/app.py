e8fffde9319202241a44b5f91d34c71afde86f4c
Anthony Ndegwa <ndegwaanthony300@gmail.com>
2025-12-03 16:54:33 +0300

    added the port

--- flask-docgen/app.py
+++ flask-docgen/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, jsonify
 from openai import OpenAI
 from config import Config
@@ -110,4 +111,5 @@ def api_generate():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
